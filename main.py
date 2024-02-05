694dc77bea4e2b6839d338f94444d2c5518c4c73
Polina.Ya <polinaya777@gmail.com>
2024-02-05 02:24:36 +0100

    update project

--- main.py
+++ main.py
@@ -1,34 +1,85 @@
+from requests import Session
 from src.scraper import WikipediaScraper
 
 
-def init_wikiScraper():
-    pass
+def create_wikiScraper():
+    '''Create the WikipediaScraper object instance'''
+    while True:
+        user_input = input('Do you want to scrape data about world leaders? (Y/N) ').lower()
+        if user_input == 'y':
+            base_url = 'https://country-leaders.onrender.com'
+            wikiScraper = WikipediaScraper(base_url, Session())
+            status_code = wikiScraper.check_connection()
+            if status_code == 200:
+                return wikiScraper
+            else:
+                print('The server is not responding. Please try again later.\n')
+                break
+        elif user_input == 'n':
+            exit()
+        else:
+            print('Please enter Y or N!\n')
+    
+
+def grabbing_data(wikiScraper: WikipediaScraper):
+    '''Grab data from the API'''
+    print('Grabbing data...')
+    try:
+        countries = wikiScraper.get_countries()
+        for country in countries:
+            wikiScraper.get_leaders(country)
+        for country, leaders in wikiScraper.leaders_data.items():
+            for leader in leaders:
+                leader['wikipedia_first_paragraph'] = wikiScraper.get_first_paragraph(leader['wikipedia_url'])
+        print('Data grabbed successfully!')
+    except:
+        print('An error occurred while grabbing the data. Please try again later.')
 
 
 def welcome():
-    print("""Welcome to the Wikipedia Scraper!
-          """)
+    '''Display the welcome message'''
+    print("""
+    Welcome to the Wikipedia Scraper!
+This script will help you grab information about
+political leaders of several countries of the world
+from API and Wikipedia and save it in JSON or CSV file.\n""")
 
 
 def menu():
     '''Display the menu'''
-    menu = """Please select one of the following options:
-    1 - Scrape a country.
-    2 - Scrape all countries.
-    3 - Get a leader.
-    4 - Get all leaders.
-    5 - Export data to JSON file (leaders_data.json).
-    6 - Exit.
-
+    menu = """You can chose the format of the file to export the data to:
+    1 - JSON
+    2 - SCV
+    3 - Exit without saving
     Your selection: """
     return menu
 
 
 def main():
+    '''Main function'''
     welcome()
-    wikiScraper = init_wikiScraper()
-
-    pass
+    wikiScraper = create_wikiScraper()
+    grabbing_data(wikiScraper)
+    while True:
+        try:
+            user_choice = int(input(menu()))
+            if user_choice == 1:
+                print('Exporting data to JSON...')
+                wikiScraper.to_json_file('leaders_data.json')
+                print('Data exported successfully to: "leaders_data.json"')
+                break
+            elif user_choice == 2:
+                print('Exporting data to CSV...')
+                wikiScraper.to_csv_file('leaders_data.csv')
+                print('Data exported successfully to: "leaders_data.csv"')
+                break
+            elif user_choice == 3:
+                print('Exiting without saving...')
+                break
+            else:
+                print('Please select a valid option!')
+        except:
+            print('Please select a valid option!')
 
 
 if __name__ == "__main__":
--- src/scraper.py
+++ src/scraper.py
@@ -1,35 +1,76 @@
-import b4 as BeautifulSoup
+from bs4 import BeautifulSoup
+from requests import Session
+import csv
 import json
+import re
 import requests
 
 class WikipediaScraper:
-    def __init__(self, url):
-        self.url = url
-        base_url = 'https://country-leaders.onrender.com'
-        country_endpoint = '/countries'
-        leaders_endpoint = '/leaders'
-        cookies_endpoint = '/cookie'
-        leaders_data = {}
-        cookie: object
+    '''A class to scrape Wikipedia for countries leaders.'''
+
+    def __init__(self, base_url: str, session: Session):
+        '''Initialize the WikipediaScraper object.'''
+        self.base_url = base_url
+        self.status_endpoint = '/status'
+        self.country_endpoint = '/countries'
+        self.leaders_endpoint = '/leaders'
+        self.cookies_endpoint = '/cookie'
+        self.session = session
+        self.leaders_data = {}
+        
+    def check_connection(self):
+        '''Check the connection to the API.'''
+        print("Checking API status...")
+        try:
+            r = self.session.get(f'{self.base_url}{self.status_endpoint}')
+            if r.status_code == 200:
+                print("The server is up and running. Continue...")
+            else:
+                print(f'Something went wrong: status code is {r.status_code}')
+            return r.status_code
+        except requests.exceptions.RequestException as e:
+            print(f'An error during request: {e}')
     
-    
-    def refresh_cookie(self) -> object:
-        self.cookie = requests.get(self.base_url + self.cookies_endpoint).json()
+    def refresh_cookie(self):
+        '''Refresh the cookie used to access the API.'''
+        try:
+            cookie = self.session.get(f'{self.base_url}{self.cookies_endpoint}').cookies
+            return cookie
+        except requests.exceptions.RequestException as e:
+            print(f'An error during request cookies: {e}')
 
     def get_countries(self):
-        response = requests.get(self.base_url + self.country_endpoint, cookies=self.cookie)
-        return response.json()
+        '''Get a list of supported countries from the API.'''
+        r = self.session.get(f"{self.base_url}{self.country_endpoint}", cookies=self.refresh_cookie())
+        return r.json()
     
-    def get_leaders(self, country: str) -> None:
-        response = requests.get(self.base_url + self.leaders_endpoint + '/' + country, cookies=self.cookie)
-        self.leaders_data[country] = response.json()
+    def get_leaders(self, country: str):
+        '''Get leaders data for a given country.'''
+        r = self.session.get(f'{self.base_url}{self.leaders_endpoint}', params={'country' : country}, cookies=self.refresh_cookie())
+        self.leaders_data[country] = r.json()
 
-    def get_first_paragraph(self, wikipedia_url: str) -> str:
-        html = requests.get(wikipedia_url).text
-        soup = BeautifulSoup(html, 'html.parser')
-        paragraphs = soup.find_all('p')
-        return paragraphs[1].text
-    
-    def to_json_file(self, filepath: str) -> None:
-        with open(filepath, 'w') as file:
-            file.write(json.dumps(self.leaders_data))  
+    def get_first_paragraph(self, wikipedia_url: str):
+        '''Get the first paragraph of the Wikipedia page for a country leader.'''
+        r = self.session.get(wikipedia_url)
+        soup = BeautifulSoup(r.text, 'html.parser')
+        div = soup.find('div', attrs={'class':"mw-body-content"})
+        paragraphs = div.find_all('p')
+        for paragraph in paragraphs:
+            if re.search(r'^<p><b>.*</b>', str(paragraph)):
+                return paragraph.text
+
+    def to_json_file(self, filepath: str):
+        '''Write the leaders_data to a JSON file.'''
+        with open(filepath, 'w', encoding='utf-8') as json.file:
+            json.dump(self.leaders_data, json.file, ensure_ascii=False, indent=4)
+
+    def to_csv_file(self, filepath: str):
+        '''Write the leaders_data to a CSV file.'''
+        field_names = ["Country", "ID", "First Name", "Last Name", "Birth Date", "Death Date",
+                    "Place of Birth", "URL", "Start Mandate", "End Mandate", "Wiki First Paragraph"]
+        with open(filepath, 'w', newline='', encoding='utf-8') as csv.file:
+            writer = csv.DictWriter(csv.file, fieldnames=field_names)
+            writer.writeheader()
+            for country, leaders in self.leaders_data.items():
+                for leader in leaders:
+                    writer.writerow(country, **leader)
